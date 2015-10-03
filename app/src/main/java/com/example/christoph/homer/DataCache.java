e692f85f7c21206c93ad09b8f97e12d150f86136
mohlerm <github@mohlerm.ch>
2015-10-03 20:28:06 +0200

    rearranged welcome screen

--- app/src/main/java/com/example/christoph/homer/DataCache.java
+++ app/src/main/java/com/example/christoph/homer/DataCache.java
@@ -1,43 +1,43 @@
-package com.example.christoph.homer;
-
-import android.content.Context;
-
-import java.io.File;
-import java.io.FileOutputStream;
-
-
-/**
- * Created by Christoph on 03.10.2015.
- */
-public class DataCache {
-
-    int nrOfFiles = 10;
-    String[] path = new String[nrOfFiles];
-    File[] file = new File[nrOfFiles];
-
-    public DataCache(Context context) {
-
-        for (int i = 0; i < nrOfFiles; i++) {
-            path[i] = context.getCacheDir().getAbsolutePath() + ""
-            file[i] = new File(path[i]);
-        }
-        if (file[i].exists()) {
-            // File exists
-        } else {
-            // File does not exist
-        }
-    }
-
-    String FILENAME = "hello_file";
-    String string = "hello world!";
-
-    FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
-    fos.write(string.getBytes());
-    fos.close();
-
-    File file = getCacheDir();
-
-}
-
-
-}
+//package com.example.christoph.homer;
+//
+//import android.content.Context;
+//
+//import java.io.File;
+//import java.io.FileOutputStream;
+//
+//
+///**
+// * Created by Christoph on 03.10.2015.
+// */
+//public class DataCache {
+//
+//    int nrOfFiles = 10;
+//    String[] path = new String[nrOfFiles];
+//    File[] file = new File[nrOfFiles];
+//
+//    public DataCache(Context context) {
+//
+//        for (int i = 0; i < nrOfFiles; i++) {
+//            path[i] = context.getCacheDir().getAbsolutePath() + ""
+//            file[i] = new File(path[i]);
+//        }
+//        if (file[i].exists()) {
+//            // File exists
+//        } else {
+//            // File does not exist
+//        }
+//    }
+//
+//    String FILENAME = "hello_file";
+//    String string = "hello world!";
+//
+//    FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+//    fos.write(string.getBytes());
+//    fos.close();
+//
+//    File file = getCacheDir();
+//
+//}
+//
+//
+//}
