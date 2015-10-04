f3e73eb400f78b2a509bd0f6abfe6ff20e84f7c0
mohlerm <github@mohlerm.ch>
2015-10-04 03:38:47 +0200

    Tweaked AnswerParser a bit

--- app/src/main/java/com/example/christoph/homer/AnswerParser.java
+++ app/src/main/java/com/example/christoph/homer/AnswerParser.java
@@ -26,60 +26,52 @@ public class AnswerParser {
         return null;
     }
     private Apartment[][] parseJSON(JSONObject jsonObject) {
-        jsonObject.length();
-        Apartment[][] apartments = new Apartment[4][4];
-//        MaterialLargeImageCard.DrawableExternal drawable = new MaterialLargeImageCard.DrawableExternal() {
-//            @Override
-//            public void setupInnerViewElements(ViewGroup parent, View viewImage) {
-//
-//                Picasso.with(getActivity())
-//                        .load("https://lh5.googleusercontent.com/-squZd7FxR8Q/UyN5UrsfkqI/AAAAAAAAbAo/VoDHSYAhC_E/s96/new%2520profile%2520%25282%2529.jpg")
-//                        .error(R.drawable.ic_error_loadingsmall)
-//                        .into((ImageView) viewImage);
-//            }
-//        }
-        try {
+        if(jsonObject!=null) {
+            Apartment[][] apartments = new Apartment[4][4];
+            try {
 
-            for (int i = 0; i < 4; i++) {
-                JSONObject root = null;
-                switch (i) {
-                    case 0:
-                        root = jsonObject.getJSONObject("current");
-                        apartments[i][0] = parseApartment(root);
-                        break;
-                    case 1:
-                        root = jsonObject.getJSONObject("another");
-                        break;
-                    case 2:
-                        root = jsonObject.getJSONObject("cheaper");
-                        break;
-                    case 3:
-                        root = jsonObject.getJSONObject("closer");
-                        break;
-                }
-                if(root == null) {
-                    throw new JSONException("Fail");
-                }
+                for (int i = 0; i < 4; i++) {
+                    JSONObject root = null;
+                    switch (i) {
+                        case 0:
+                            root = jsonObject.getJSONObject("current");
+                            apartments[i][0] = parseApartment(root);
+                            break;
+                        case 1:
+                            root = jsonObject.getJSONObject("another");
+                            break;
+                        case 2:
+                            root = jsonObject.getJSONObject("cheaper");
+                            break;
+                        case 3:
+                            root = jsonObject.getJSONObject("closer");
+                            break;
+                    }
+                    if (root == null) {
+                        throw new JSONException("Fail");
+                    }
 
-                if(i!=0) {
-                    apartments[i][0] = parseApartment(root.getJSONObject("current"));
-                    JSONObject another = root.getJSONObject("another");
-                    apartments[i][1] = parseApartment(another);
-                    JSONObject cheaper = root.getJSONObject("cheaper");
-                    apartments[i][2] = parseApartment(cheaper);
-                    JSONObject closer = root.getJSONObject("closer");
-                    apartments[i][3] = parseApartment(closer);
-                }
+                    if (i != 0) {
+                        apartments[i][0] = parseApartment(root.getJSONObject("current"));
+                        JSONObject another = root.getJSONObject("another");
+                        apartments[i][1] = parseApartment(another);
+                        JSONObject cheaper = root.getJSONObject("cheaper");
+                        apartments[i][2] = parseApartment(cheaper);
+                        JSONObject closer = root.getJSONObject("closer");
+                        apartments[i][3] = parseApartment(closer);
+                    }
 
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
+            return apartments;
+        } else {
+            return null;
         }
-
-        return apartments;
     }
     private Apartment parseApartment(JSONObject jsonObject) throws JSONException {
-        int id; int price; int traveltime; String title; String img; String address; Float rooms;
+        int id; int price; int traveltime; String title; String img; String address; String rooms;
         try {
             id = jsonObject.getInt("id");
         } catch (JSONException e) {
@@ -120,9 +112,13 @@ public class AnswerParser {
             title = "no title available";
         }
         try {
-            rooms = (float) jsonObject.getDouble("rooms");
+            float fr = (float)jsonObject.getDouble("rooms");
+            if(fr==0.0f) {
+                rooms = "n/a";
+            }
+            rooms = String.valueOf(fr);
         } catch (JSONException e) {
-            rooms = -1.0f;
+            rooms = "n/a";
         }
         return new Apartment(id, price, traveltime, title, "", img, address, rooms, 0);
     }
@@ -133,9 +129,6 @@ public class AnswerParser {
             return jsonObject.getString("sessionid");
         } catch (JSONException e) {
             e.printStackTrace();
-        }
-        if(jsonObject.length() >0) {
-
         }
         return "";
     }
